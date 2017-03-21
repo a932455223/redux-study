43f6c45eea10245fe7e8075075c130e1072eea4f
simon.du <a932455223@qq.com>
2017-03-21 22:31:25 +0800

    some update

---
+++ todomvc/src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ const App = () => (
+		<div>
+			<header className="header">
+				<h1>todos</h1>
+				<input className="new-todo" placeholder="What needs to be done?" />
+			</header>
+			<section className="main" >
+				
+			</section>
+		</div>
+	)
+
+export default App
---
+++ todomvc/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import 'todomvc-app-css/index.css'
+import App from './containers/App'
+ReactDom.render(<App />, document.getElementById('root'))
