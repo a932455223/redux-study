b2e5204a4b6ddc29d01ec068ad13d93dd5b0a469
simon.du <a932455223@qq.com>
2017-03-29 23:43:01 +0800

    add components

---
+++ components/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+function info(msg,color='red'){
+    console.log('%c'+msg,'color:'+color);
+}
+class Message extends Component{
+
+    componentWillReceiveProps(){
+        info('I will receive.')
+    }
+
+    componentWillUpdate(){
+        info('I will update.')
+    }
+
+    componentDidUpdate(){
+        info('I updated.')
+    }
+    render(){
+        info('I rendering.')
+        return (
+            <h1>Receive test.</h1>
+        )
+    }
+}
+
+class App extends Component {
+  state = {
+      number:0
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={()=>{this.setState({number:this.state.number+1})}}>test</button>
+        <p>{this.state.number}</p>
+        <Message />
+      </div>
+    );
+  }
+}
+
+export default App;
